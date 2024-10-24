1755cf155fce7a2cdb7d8fc8d39078dcaab2f8c7
Khairunnisanuraqila <khairunnisanuraqila121@gmail.com>
2024-10-24 08:44:49 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,24 @@
-public class Nomor2{
-    public static void main(String[] args){
 
-    }
+import java.util.Scanner;
+
+
+public class driver{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        // namaDriver 
+        System.out.println("masukan nama driver");
+        String nama = scanner.nextLine(); 
+        System.out.println("lama perjalanan trip");
+        String trip = scanner.nextLine();
+        
+
+        // menghitung total perkilo meter trip
+        System.out.println("total perkilo meter tripbeliau");
+        int totalBiaya = 13000;
+        int totalPerkilo = 13000;
+       
+        }
+                       
+    
 }
