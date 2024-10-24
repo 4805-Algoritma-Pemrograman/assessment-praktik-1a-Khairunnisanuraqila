b05fc67269a154115cff078baeb33c8387a54f3a
Khairunnisanuraqila <khairunnisanuraqila121@gmail.com>
2024-10-24 09:07:21 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
-    }
+public class honorLembur {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan total jam lembur: ");
+        int jamLembur = scanner.nextInt();
+        double honorLembur;
+
+
+        if (jamLembur <= 16) {
+            honorLembur = jamLembur * 110000;
+        } else {
+            // Honor lembur untuk 16 jam pertama
+            double honor16Jam = 16 * 10000;
+            // Hitung sisa jam lembur
+            
+        }
+
+ 
+        
 }
